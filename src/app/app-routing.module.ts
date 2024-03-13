844c4e75ce5cb737486bc0d6d79d82a6076766fc
Mario Gutiérrez <marioguti033@gmail.com>
2024-03-13 18:54:46 +0100

    Login y Register definitivos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { RegisterComponent } from './register/register.component';
+import { AdminPanelComponent } from './admin-panel/admin-panel.component';
+import { HistorialUsuariosComponent } from './historial-usuarios/historial-usuarios.component';
+import { GestionUsuariosComponent } from './gestion-usuarios/gestion-usuarios.component';
 
 const routes: Routes = [
   {path: '', component: LandingPageComponent},
   {path: 'login', component: LoginPageComponent},
-  {path: 'register', component: RegisterComponent}
+  {path: 'register', component: RegisterComponent},
+  {path: 'admin-panel', component: AdminPanelComponent},
+  {path: 'admin-panel/historial-usuarios', component: HistorialUsuariosComponent},
+  {path: 'admin-panel/gestion-usuarios', component: GestionUsuariosComponent}
 ];
 
 @NgModule({
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -14,4 +14,5 @@ export class RegisterComponent {
 
 	model!: NgbDateStruct;
 	date!: { year: number; month: number };
+
 }
