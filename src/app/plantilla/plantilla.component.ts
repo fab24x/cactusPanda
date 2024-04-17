c6f89a4d64601d7e8274d369e915c2c3c24a5b14
Mario Gutiérrez <marioguti033@gmail.com>
2024-04-17 13:18:00 +0200

    Color primary green

---
+++ src/app/plantilla/plantilla.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-plantilla',
+  templateUrl: './plantilla.component.html',
+  styleUrl: './plantilla.component.scss'
+})
+export class PlantillaComponent {
+
+}
