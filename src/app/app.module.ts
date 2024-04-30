8ccec2ff13a155e71dda489644e80abe02f6a745
Mario Gutiérrez <marioguti033@gmail.com>
2024-04-30 22:28:08 +0200

    form validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,14 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideProtractorTestingSupport } from '@angular/platform-browser';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './header/header.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
-import { RouterModule } from '@angular/router';
-import { RegisterComponent } from './register/register.component';
-import { AdminPanelComponent } from './admin-panel/admin-panel.component';
-import { HistorialUsuariosComponent } from './historial-usuarios/historial-usuarios.component';
-import { SoporteTecnicoComponent } from './soporte-tecnico/soporte-tecnico.component';
-import { GestionUsuariosComponent } from './gestion-usuarios/gestion-usuarios.component';
-import { MatDialog, MatDialogModule } from '@angular/material/dialog';
-import { ModalModule } from 'ngx-bootstrap/modal';
+import { RegisterComponent } from './register/register.component'; // Importa RegisterComponent
 import { FooterComponent } from './footer/footer.component';
-import { UserPanelComponent } from './user-panel/user-panel.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { ContactoComponent } from './contacto/contacto.component';
-import { PlantillaComponent } from './plantilla/plantilla.component';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -28,24 +16,17 @@ import { PlantillaComponent } from './plantilla/plantilla.component';
     HeaderComponent,
     LandingPageComponent,
     LoginPageComponent,
-    FooterComponent,
-    AdminPanelComponent,
-    UserPanelComponent,
-    PageNotFoundComponent,
-    ContactoComponent,
-    PlantillaComponent
+    // RegisterComponent, // Agrega RegisterComponent a la lista de declaraciones
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     NgbModule,
-    RouterModule,
     AppRoutingModule,
-    MatDialogModule,
-    ModalModule.forRoot(),
-
+    FormsModule,
+    ReactiveFormsModule // Agrega ReactiveFormsModule a la lista de imports
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-
 export class AppModule { }
--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,10 +1,61 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrl: './login-page.component.scss'
+  styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent {
+  camposVacios = false;
+  camposIncorrectos = false;
+  title = 'Angular Form Validation Tutorial';
+  angForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    // Inicializar angForm en el constructor
+    this.angForm = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    // Verificar si el formulario es válido
+    if (this.angForm.valid) {
+      // Aquí puedes agregar la lógica para enviar los datos del formulario
+      console.log('Formulario válido');
+      console.log('Datos del formulario:', this.angForm.value);
+    } else {
+      // Si el formulario no es válido, ver si los campos están vacíos
+      const emailControl = this.angForm.get('email');
+      const passwordControl = this.angForm.get('password');
+  
+      if (!emailControl || !passwordControl || !emailControl.value || !passwordControl.value) {
+        this.camposVacios = true;
+        this.camposIncorrectos = false;
+        return;
+      }
+  
+      // Si los campos no están vacíos, establecer camposIncorrectos en true
+      this.camposVacios = false;
+      this.camposIncorrectos = true;
+      // También puedes marcar los campos como tocados para mostrar mensajes de error en la plantilla HTML
+      this.markFormGroupTouched(this.angForm);
+    }
+  }
+  
+  
+  
+
+  // Método para marcar todos los campos como tocados
+  private markFormGroupTouched(formGroup: FormGroup) {
+    Object.values(formGroup.controls).forEach(control => {
+      control.markAsTouched();
+
+      if (control instanceof FormGroup) {
+        this.markFormGroupTouched(control);
+      }
+    });
+  }
 }
