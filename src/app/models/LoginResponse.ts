abbce85b286d3ad7a415d447b1ce90b094a93084
Mario Gutiérrez <marioguti033@gmail.com>
2024-05-27 20:41:09 +0200

    Register

--- src/app/models/LoginResponse.ts
+++ src/app/models/LoginResponse.ts
@@ -1,5 +1,13 @@
 export interface LoginResponse {
-    access_token: string;
-    token_type: string;
-    expires_in: number;
-  }
+  access_token: string;
+  token_type: string;
+  expires_in: number;
+  user: {
+    nombre_de_usuario: string;
+    correo: string;
+    fecha_nacimiento: string;
+    nombre: string;
+    apellido: string;
+    id: number;
+  };
+}
