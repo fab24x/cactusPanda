2a25169cd85a640603ff1f6632033168133ee6a9
Christian Jonathan Yang Gonzalez <christianj_yang@icloud.com>
2024-05-29 19:43:56 +0200

    tabla ranking con sus predicciones

--- src/app/models/predi-jugador.ts
+++ src/app/models/predi-jugador.ts
@@ -1,19 +1,27 @@
 export class PrediJugador {
-    id_jugador: number;
-    jornada: number;
-    puntos_jornada: number;
-    nombre_del_jugador: string;
-
+  id_jugador: number;
+  jornada: number;
+  puntos_jornada: number;
+  nombre_del_jugador: string;
+  puntos_jornada_prediccion: number;
+  precio_jugador: number;
+  prediccion_precio_jugador: number;
 
   constructor(
-    id_jugador: number,
-    jornada: number,
-    puntos_jornada: number,
-    nombre_del_jugador: string,
+      id_jugador: number,
+      jornada: number,
+      puntos_jornada: number,
+      nombre_del_jugador: string,
+      puntos_jornada_prediccion: number,
+      precio_jugador: number,
+      prediccion_precio_jugador: number
   ) {
-    this.id_jugador = id_jugador;
-    this.nombre_del_jugador = nombre_del_jugador;
-    this.jornada = jornada;
-    this.puntos_jornada = puntos_jornada;
+      this.id_jugador = id_jugador;
+      this.nombre_del_jugador = nombre_del_jugador;
+      this.jornada = jornada;
+      this.puntos_jornada = puntos_jornada;
+      this.puntos_jornada_prediccion = puntos_jornada_prediccion;
+      this.precio_jugador = precio_jugador;
+      this.prediccion_precio_jugador = prediccion_precio_jugador;
   }
 }
